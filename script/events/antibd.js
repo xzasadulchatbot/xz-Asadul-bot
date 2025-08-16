54c9c33bd72e29fd1532d3c33493fccc717ad356
xzasadulchatbot <asadulgw833@gmail.com>
2025-08-16 12:38:37 +0600

    Update antibd.js

--- script/events/antibd.js
+++ script/events/antibd.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "antibd",
   eventType: ["log:user-nickname"],
   version: "0.0.1",
-  credits: "𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 𝗯𝗼𝘁",
+  credits: "xz Asadul chat bot",
   description: "Against changing Bot's nickname"
 };
 
